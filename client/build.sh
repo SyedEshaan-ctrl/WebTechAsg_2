73a2ef6bd4b38aca9087b51b8b42bce089c9858e
SyedEshaan-ctrl <eshaan2hyd@gmail.com>
2025-04-27 01:27:11 +0530

    .

--- client/build.sh
+++ client/build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Fix permission issues
-echo "Setting permissions for react-scripts"
-chmod -R +x node_modules/.bin/
-echo "Running build with CI=false"
+npm ci
 CI=false npx react-scripts build

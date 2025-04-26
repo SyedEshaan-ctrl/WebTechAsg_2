f05abe6279dbc56fcfc3a0ddf1023e0944050ea0
SyedEshaan-ctrl <eshaan2hyd@gmail.com>
2025-04-26 22:53:10 +0530

    Update studentService.js

--- client/src/services/studentService.js
+++ client/src/services/studentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/students';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/students';
 
 // Get all students
 export const getAllStudents = async () => {
